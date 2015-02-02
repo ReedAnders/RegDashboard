bf7a61c305f180291eaeedea8da7f4f10b2db4d3
ReedAnders <rean8874@colorado.edu>
2015-02-02 12:58:35 -0700

    refactoring names

---
+++ src/boot.js
@@ -0,0 +1,17 @@
+/**
+ * Created by reedanderson on 2/2/15.
+ */
+head.load(
+    { file: "components/angular/angular.min.js" },
+    { file: "components/angular-cookies/angular-cookies.min.js" },
+    { file: "components/angular-ui-router/release/angular-ui-router.min.js" },
+    { file: "js/module.js" },
+    { file: "js/routes.js" },
+    { file: "js/controllers/master-ctrl.js"},
+    { file: "js/controllers/alert-ctrl.js" },
+    { file: "js/directives/loading.js" },
+    { file: "js/directives/widget.js" },
+    { file: "js/directives/widget-body.js" },
+    { file: "js/directives/widget-footer.js" },
+    { file: "js/directives/widget-header.js" }
+);
