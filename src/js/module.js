93005121b6ee950e50ddcfbd6348c5b31d9ecf7f
ReedAnders <rean8874@colorado.edu>
2015-02-02 12:38:18 -0700

    first commit

---
+++ src/js/module.js
@@ -0,0 +1,1 @@
+angular.module('RegDashboard', ['ui.bootstrap', 'ui.router', 'ngCookies']);
---
+++ src/js/routes.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * Route configuration for the RegDashboard module.
+ */
+angular.module('RegDashboard').config(['$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+
+        // For unmatched routes
+        $urlRouterProvider.otherwise('/');
+
+        // Application routes
+        $stateProvider
+            .state('index', {
+                url: '/',
+                templateUrl: 'templates/dashboard.html'
+            })
+            .state('tables', {
+                url: '/tables',
+                templateUrl: 'templates/tables.html'
+            });
+    }
+]);
