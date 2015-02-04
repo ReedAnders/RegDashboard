909a569c461677c9db31b049dad70bbee58c8b9f
ReedAnders <rean8874@colorado.edu>
2015-02-04 12:45:28 -0700

    adding boot.js

--- client/assets/js/boot.js
+++ client/assets/js/boot.js
@@ -2,16 +2,24 @@
  * Created by reedanderson on 2/2/15.
  */
 head.load(
-    //{ file: "lib/angular/angular.min.js" },
-    //{ file: "lib/angular-cookies/angular-cookies.min.js" },
-    //{ file: "lib/angular-ui-router/release/angular-ui-router.min.js" },
-    //{ file: "app/app.js" },
-    //{ file: "app/routes.js" },
-    //{ file: "app/controllers/MainController.js"},
-    //{ file: "app/controllers/AlertsController.js" },
-    //{ file: "app/directives/loading.js" },
-    //{ file: "app/directives/widget.js" },
-    //{ file: "app/directives/widget-body.js" },
-    //{ file: "app/directives/widget-footer.js" },
-    //{ file: "app/directives/widget-header.js" }
+
+    { file: "lib/angular/angular.min.js" },
+    { file: "lib/angular-bootstrap/ui-bootstrap-tpls.min.js" },
+    { file: "lib/angular-cookies/angular-cookies.min.js" },
+    { file: "lib/angular-ui-router/release/angular-ui-router.min.js" },
+    { file: "src/regDashboard/RegDashboard.js" },
+    { file: "src/regDashboard/app/app.js" },
+    { file: "src/regDashboard/app/routes.js" },
+    { file: "src/regDashboard/app/controllers/MainController.js" },
+    { file: "src/regDashboard/app/controllers/AlertsController.js" },
+    { file: "src/regDashboard/app/directives/loading.js" },
+    { file: "src/regDashboard/app/directives/widget.js" },
+    { file: "src/regDashboard/app/directives/widget-body.js" },
+    { file: "src/regDashboard/app/directives/widget-footer.js" },
+    { file: "src/regDashboard/app/directives/widget-header.js" },
+    { file: "src/regDashboard/app/models/regModel.js" },
+    { file: "src/regDashboard/dashboard/dashboard.js" },
+    { file: "src/regDashboard/dashboard/controllers/DashboardCtrl.js" },
+    { file: "src/regDashboard/dashboard/directives/ChartDirective.js" }
+
 );
