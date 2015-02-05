59b24ba1e38b2ffbaf13a1aa17797cc0094c983e
ReedAnders <rean8874@colorado.edu>
2015-02-05 11:10:43 -0700

    refactoring alerts

--- client/src/RegDashboard/app/controllers/AlertsController.js
+++ client/src/RegDashboard/app/controllers/AlertsController.js
@@ -5,9 +5,9 @@
 angular
     .module('RegDashboard.Common')
     .controller('AlertsCtrl', function ($cookieStore) {
-        var rdAlerts = this;
+        var AlertsCtrl = this;
 
-        rdAlerts.alerts = [{
+        AlertsCtrl.alerts = [{
             type: 'success',
             msg: 'Thanks for visiting! Feel free to create pull requests to improve the dashboard!'
         }, {
@@ -15,17 +15,17 @@ angular
             msg: 'Found a bug? Create an issue with as many details as you can.'
         }];
 
-        rdAlerts.addAlert = function() {
+        AlertsCtrl.addAlert = function() {
             rdAlerts.alerts.push({
                 msg: 'Another alert!'
             });
         };
 
-        rdAlerts.closeAlert = function(index) {
-            rdAlerts.alerts.splice(index, 1);
+        AlertsCtrl.closeAlert = function(index) {
+            AlertsCtrl.alerts.splice(index, 1);
         };
 
         //TODO add $cookieStore for alerts
-        rdAlerts.hideAlerts = true;
+        AlertsCtrl.hideAlerts = false;
 
     });
